814608cc2a687d3b915f7d46e96eacdede50b55c
FragaEmanuel <emanuel.fraga@correo.ucu.edu.uy>
2024-08-28 10:03:42 -0300

    Clase board

---
+++ src/Program/Board.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+
+namespace Ucu.Poo.GameOfLife;
+
+public class Board
+{
+    private bool[,] board; //variable que representa el tablero
+    
+    public void SetValue(int i, int j, bool value) //añadir valor
+    {
+        board[i, j] = value;
+    }
+    
+    public bool GetValue(int i, int j)              //pedir valor
+    {
+        return board[i,j];
+    }
+
+    public int Width()                              //metodo ancho
+    {
+        return board.GetLength(0);
+    }
+    
+    public int Height()                             //metodo ancho
+    {
+        return board.GetLength(1);
+    }
+
+}
