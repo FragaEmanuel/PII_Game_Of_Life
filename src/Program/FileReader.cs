0bfe16b35f09040fb3a6ce3fcc0692a94f32b1f5
FragaEmanuel <emanuel.fraga@correo.ucu.edu.uy>
2024-08-29 09:27:36 -0300

    Esto deberia funcionar

--- src/Program/FileReader.cs
+++ src/Program/FileReader.cs
@@ -5,14 +5,16 @@ namespace Ucu.Poo.GameOfLife;
 public class FileReader
 {
     string url = @"..\..\assets";
+
+    public Board Return()
+    {
     string content = File.ReadAllText(url);
     string[] contentLines = content.Split('\n');
-    private Board board = new Board[contentLines.Length, contentLines[0].Length];
-    int height = board.Height;
-    int width = board.Width;
-        for (int i=0; i< width; i++)
-     {
-        for (int j=0; j< board.Height; j++)
+    Board board = new Board[contentLines.Length, contentLines[0].Length];
+        
+    for (int i=0; i< contentLines.Length; i++)
+    {
+        for (int j=0; j< contentLines[0].Length; j++)
         {
             if(board[i]=='1')
             {
@@ -20,4 +22,5 @@ public class FileReader
             }
         }
     }
-}
+    }     
+}   
