9936e9f920f2d4d245e8bd8aeab18c81b43dcc9d
FragaEmanuel <emanuel.fraga@correo.ucu.edu.uy>
2024-08-30 20:08:02 -0300

    filereader funciona.. creo

--- src/Program/FileReader.cs
+++ src/Program/FileReader.cs
@@ -10,14 +10,13 @@ public class FileReader
     {
         string content = File.ReadAllText(url);
         string[] contentLines = content.Split('\n');
-        bool[,] archivo = new bool[contentLines.Length, contentLines[0].Length];
         Board board = new Board(contentLines.Length, contentLines[0].Length);
 
         for (int i = 0; i < contentLines.Length; i++)
         {
             for (int j = 0; j < contentLines[0].Length; j++)
             {
-                if ((archivo(i,j)) == '1')
+                if (contentLines[i][j] == '1') 
                 {
                     board.SetValue(i,j,true);
                 }
