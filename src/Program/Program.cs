bfd3e7e39c050591899d9d93dea9a253bdb58f4f
Thiago Castillo <136084200+thiagocastillo@users.noreply.github.com>
2024-08-30 20:03:21 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,22 @@
-﻿using System;
+using System;
+using System.Threading;
 
 namespace Ucu.Poo.GameOfLife
 {
     class Program
     {
-        ////Programa
-        /// 1. leer el archivo
-        /// 2. iniciar un while donde se actualiza el tablero en base a la logica e imprime continuamente
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader(); // Crea una instancia de FileReader.
+            bool[,] board = fileReader.Return(); // Lee el archivo y obtiene la matriz de booleanos.
+
+            while (true)
+            {
+                Console.Clear();
+                Printer.PrintBoard(board); // Imprime el tablero en la consola.
+                board = GameLogic.ProcessNextGeneration(board); // Calcula la siguiente generación del tablero.
+                Thread.Sleep(300); // Pausa para ver el cambio.
+            }
+        }
     }
 }
